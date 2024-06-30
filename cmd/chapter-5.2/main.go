e8f4c8678559f18bc1023bfa60dd1d454de2402f
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-29 21:38:41 -0400

    Finished exercise 5.2

--- cmd/chapter-5.2/main.go
+++ cmd/chapter-5.2/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func fileLen(filename string) (int, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer file.Close()
+
+	data := make([]byte, 2048)
+
+	var totalCount int
+
+	for {
+
+		count, err := file.Read(data)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return 0, nil
+		}
+
+		totalCount += count
+	}
+
+	return totalCount, nil
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	filename := os.Args[1]
+	count, err := fileLen(filename)
+
+	if err != nil {
+		fmt.Printf("An error occurred \"%s\"\n", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("The length of the file is", count)
 }
