e95272ed2c0cbe5da406797acd1f51a7303340f9
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-29 21:25:29 -0400

    Updated to handle divide by zero

--- cmd/chapter-5.1/main.go
+++ cmd/chapter-5.1/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+type opFuncType func(int, int) (int, error)
+
+func add(i int, j int) (int, error) { return i + j, nil }
+
+func sub(i int, j int) (int, error) { return i - j, nil }
+
+func mul(i int, j int) (int, error) { return i * j, nil }
+
+func div(i int, j int) (int, error) {
+	if j == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return i / j, nil
+}
+
+var opMap = map[string]opFuncType{
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	expressions := [][]string{
+		{"2", "+", "3"},
+		{"2", "-", "3"},
+		{"2", "*", "3"},
+		{"2", "/", "3"},
+		{"2", "%", "3"},
+		{"two", "+", "three"},
+		{"5"},
+		{"5", "/", "0"},
+	}
+	for _, expression := range expressions {
+		if len(expression) != 3 {
+			fmt.Println("invalid expression:", expression)
+			continue
+		}
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("unsupported operator:", op)
+			continue
+		}
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result, err := opFunc(p1, p2)
+
+		if err != nil {
+			fmt.Printf("There was an error \"%s\"\n", err.Error())
+			continue
+		}
+
+		fmt.Println(result)
+	}
 }
