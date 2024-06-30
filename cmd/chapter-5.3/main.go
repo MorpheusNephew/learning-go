559885e5c9173f431ffcb3b810efcd7ee1e743e8
Jelani Jackson <jelani.jackson@zapier.com>
2024-06-29 21:42:00 -0400

    Finished exercise 5.3 prefixer

--- cmd/chapter-5.3/main.go
+++ cmd/chapter-5.3/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func prefixer(prefix string) func(string) string {
+	return func(word string) string {
+		return fmt.Sprintf("%s %s", prefix, word)
+	}
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	helloPrefix := prefixer("Hello")
+	fmt.Println(helloPrefix("Bob"))
+	fmt.Println(helloPrefix("Maria"))
 }
