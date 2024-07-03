d7d09c80125916c77c25fd434a7a1e05b1670155
Jelani Jackson <jelani.jackson@zapier.com>
2024-07-02 23:14:37 -0400

    added questions

--- cmd/chapter-6.2/main.go
+++ cmd/chapter-6.2/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+/*
+Write two functions. The `UpdateSlice` function takes in a `[]string` and a `string`.
+It sets the last position in the passed-in slice to the passed-in `string`.
+At the end of `UpdateSlice`, print the slice after making the change.
+The `GrowSlice` function also takes in a `[]string` and a `string`. It appends the `string` onto the slice.
+At the end of `GrowSlice`, print the slice after making the change.
+Call these functions from `main`.
+Print out the slice before each function is called and after each function is called.
+Do you understand why some changes are visible in `main` and why some changes are not?
+*/
 func main() {
 	fmt.Println("Hello world!")
 }
--- cmd/chapter-6.3/main.go
+++ cmd/chapter-6.3/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/*
+Write a program that builds a `[]Person` with 10,000,000 entries (they could all be the same names and ages).
+See how long it takes to run. Change the value of `GOGC` and see how that affects the time it takes for the program to complete.
+Set the environment variable `GODEBUG=gctrace=1` to see when garbage collections happen and see how changing `GOGC` changes the number of garbage collections.
+What happens if you create the slice with a capacity of 10,000,000?
+*/
 func main() {
 	fmt.Println("Hello world!")
 }
