67a2ff89f67a49e031e80dd48bcf226949bc05a8
Jelani Jackson <jelani.jackson@zapier.com>
2024-07-08 05:12:24 -0400

    Created helper functions

--- cmd/chapter-7.1/main.go
+++ cmd/chapter-7.1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 /*
 You have been asked to manage a basketball league and are going to write a program to help you.
@@ -10,10 +13,13 @@ The second type is called `League` and has a field called `Teams` for the teams 
 and a field called `Wins` that maps a team’s name to its number of wins.
 */
 func main() {
-	miamiHeat := Team{Name: "Miami Heat", Players: []string{"Jimmy Buckets"}}
-	nba := League{Teams: []string{miamiHeat.Name}, Wins: map[string]int{miamiHeat.Name: 10}}
+	miamiHeat := NewTeam("Miami Heat", "Jimmy Buckets", "Tyler Herro")
+	orlandoMagic := NewTeam("Orlando Magic", "Victor Oladipo")
+	nba := NewLeague(map[string]int{miamiHeat.Name: 10, orlandoMagic.Name: 3}, miamiHeat.Name, orlandoMagic.Name)
 
-	fmt.Println(miamiHeat, nba, nba.Wins[miamiHeat.Name])
+	fmt.Println(nba)
+	fmt.Println(miamiHeat)
+	fmt.Println(orlandoMagic)
 }
 
 type Team struct {
@@ -21,7 +27,31 @@ type Team struct {
 	Players []string
 }
 
+func NewTeam(name string, players ...string) Team {
+	return Team{Name: name, Players: players}
+}
+
+func (t Team) String() string {
+	return fmt.Sprintf("It's your %s!\nHere are your players! %s\n", t.Name, strings.Join(t.Players, ", "))
+}
+
 type League struct {
 	Teams []string
 	Wins  map[string]int
+}
+
+func NewLeague(wins map[string]int, teams ...string) League {
+	return League{Teams: teams, Wins: wins}
+}
+
+func (l League) String() string {
+	var leagueInfo strings.Builder
+
+	leagueInfo.WriteString("Welcome to the National Basketball Association!\n\n")
+
+	for _, teamName := range l.Teams {
+		leagueInfo.WriteString(fmt.Sprintf("%s with %d wins\n", teamName, l.Wins[teamName]))
+	}
+
+	return leagueInfo.String()
 }
