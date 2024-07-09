1a36a800df70455233a8586a0ba39db481c1f4b0
Jelani Jackson <jelani.jackson@zapier.com>
2024-07-08 23:08:19 -0400

    Finished exercise 8.2

--- cmd/chapter-8.2/main.go
+++ cmd/chapter-8.2/main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+/*
+Define a generic interface called Printable that matches a type that implements fmt.Stringer and has an underlying type of int or float64.
+Define types that meet this interface. Write a function that takes in a Printable and prints its value to the screen using fmt.Println.
+*/
 func main() {
-	fmt.Println("Hello world!")
+	myPrintableInt := PrintInt(13)
+	myPrintableFloat := PrintFloat(5.03843)
+
+	Print(myPrintableInt)
+	Print(myPrintableFloat)
+}
+
+type Printable interface {
+	fmt.Stringer
+	~int | ~float64
+}
+
+type PrintInt int
+
+func (pi PrintInt) String() string {
+	return fmt.Sprintf("%d", pi)
+}
+
+type PrintFloat float64
+
+func (pf PrintFloat) String() string {
+	return fmt.Sprintf("%f", pf)
+}
+
+func Print[T Printable](printable T) {
+	fmt.Println(printable)
 }
